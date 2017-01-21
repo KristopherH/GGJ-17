58dec3224cc6829ba9ce76fe00d95dc0d477bb30
bhison <catbat04@gmail.com>
2017-01-21 22:35:52 +0000

    Spoon spawner

---
+++ Game/Assets/SpoonSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpoonSpawner : MonoBehaviour {
+	
+	static SpoonSpawner _instance;
+	public static SpoonSpawner Instance
+	{
+		get
+		{
+			if (_instance == null)
+				_instance = GameObject.FindObjectOfType<SpoonSpawner> ();
+			return _instance;
+		}
+	}
+
+	[SerializeField]
+	GameObject spoonPrefab;
+	[SerializeField]
+	int cookDelay;
+
+	int cookCountDown;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	public void RegisterCook()
+	{
+		cookCountDown--;
+		if (cookCountDown <= 0)
+			Spawn ();
+	}
+
+	void Spawn()
+	{
+		cookDelay++;
+		cookCountDown = cookDelay;
+		Instantiate (spoonPrefab, gameObject.transform.position, gameObject.transform.rotation);  
+	}
+}
