c769999f79de9ab838902aa338ea6f4acc97d13f
Gualtiero <gualtyphone@me.com>
2017-01-21 09:06:41 +0000

    1st Particles attempt

---
+++ Game/Assets/PopCorn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopCorn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.H)){
+			Shoot();
+		}
+	}
+
+	
+
+	public void Shoot(){
+		ParticleSystem[] pops = GetComponentsInChildren<ParticleSystem>();
+
+		for (int i = 0; i < pops.Length; i++){
+			pops[i].Play();
+		}
+	}
+
+}
