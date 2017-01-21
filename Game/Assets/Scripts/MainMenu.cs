e2500261f9e7b0d3aa15265176caf04f95ad240c
Benjamin Meredith <benjamin2.meredith@live.uwe.ac.uk>
2017-01-21 18:03:44 +0000

    Main Menu Made not linked

---
+++ Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    // Use this for initialization
+    // Update is called once per frame
+    private bool clicked = false; 
+    public float speed =  2.0f;
+    public Text changeText;
+    private int randomNum;
+
+    void Update () {
+        if (clicked == true)
+        {
+            transform.Translate(0,0, speed * Time.deltaTime);
+            switch (randomNum)
+            {
+                case 0:
+                    changeText.text = "RUM IS GOOD";
+                    break;
+                case 1:
+                    changeText.text = "FEED ME";
+                    break;
+                case 2:
+                    changeText.text = "GET IN ME";
+                    break;
+                case 3:
+                    changeText.text = "MEOW";
+                    break;
+                case 4:
+                    changeText.text = "!Cake";
+                    break;
+            }
+            
+        }
+   
+	}
+    public void PlayButton_Click()
+    {
+        clicked = true;
+        randomNum = Random.Range(0, 5);
+
+    }
+}
--- Game/Assets/Scripts/Rotate.cs
+++ Game/Assets/Scripts/Rotate.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class Rotate : MonoBehaviour {
 
     // Use this for initialization
+   
 
     // Update is called once per frame
     public float speed = 10f;
 	void Update () {
-
-        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+       
+             transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        
 	}
 }
