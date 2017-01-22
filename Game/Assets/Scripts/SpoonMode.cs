d0911bd617698af0ebaa92dedee6c46b5ce680a6
bhison <catbat04@gmail.com>
2017-01-22 01:32:05 +0000

    Spoon Stuff

---
+++ Game/Assets/Scripts/SpoonMode.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpoonMode : MonoBehaviour {
+
+	[SerializeField] float modeLength;
+	float timer;
+
+	PlayerAttack pa;
+
+	// Use this for initialization
+	void Start () 
+	{
+		timer = modeLength;
+		Player_Controller pc = GetComponentInParent<Player_Controller> ();
+		//pa.SetInvincible (true);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer -= Time.deltaTime;
+		if (timer <= 0.0f)
+			TimeExpired();
+	}
+
+	void ReleaseSpoon()
+	{
+		//pa.SetInvincible (false);
+		this.gameObject.SetActive (false);
+	}
+
+	void TimeExpired()
+	{
+		//Death effect
+		//GetComponentInParent<PlayerHealth> ().Kill();
+	}
+}
